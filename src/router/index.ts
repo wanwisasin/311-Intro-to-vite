b245b5858c66655db5dd9ba403c11dc428fbe927
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-07-22 19:44:07 +0700

    lab3 3.3

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import EventListView from '@/views/EventListView.vue'
 import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 import AboutView from '@/views/AboutView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 
@@ -20,21 +21,29 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
-      props: true
-    },
-    {
-      path: '/event/:id/register',
-      name: 'event-register-view',
-      component: EventRegisterView,
-      props: true
-    },
-    {
-      path: '/event/:id/edit',
-      name: 'event-edit-view',
-      component: EventEditView,
-      props: true
+      name: 'event-layout-view',
+      component: EventLayoutView,
+      props: true,
+      children: [
+        {
+          path: '',
+          name: 'event-detail-view',
+          component: EventDetailView,
+          props: true
+        },
+        {
+          path: 'register',
+          name: 'event-register-view',
+          component: EventRegisterView,
+          props: true
+        },
+        {
+          path: 'edit',
+          name: 'event-edit-view',
+          component: EventEditView,
+          props: true
+        }
+      ]
     },
     {
       path: '/about',
