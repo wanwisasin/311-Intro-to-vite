4fe9b797a5062256b4cb85111a45b19bd8811754
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-08-11 01:25:29 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { RouterLink, RouterView } from 'vue-router'
 import { useMessageStore } from './stores/message'
 import { storeToRefs } from 'pinia'
-import SpeedInsights from '@vercel/speed-insights'
 
 const store = useMessageStore()
 const { message } = storeToRefs(store)
