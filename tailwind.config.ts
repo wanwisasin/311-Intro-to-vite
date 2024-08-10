27e982a0eec0078fd7f64d6b2a4d85ff8886ab5a
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-08-10 14:23:01 +0700

    lab5

---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        sp: '0 3px 12px 0 rgba(0,0,0,0.2)'
+      }
+    }
+  },
+  plugins: []
+} satisfies Config
