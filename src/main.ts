f613403a67dbc0d2b1aecb8b047d9f6437e67555
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-08-11 00:52:03 +0700

    fixbug

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
-const app = createApp(App)
 import { inject } from '@vercel/analytics'
-inject
+
+const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
+inject()
 
 app.mount('#app')
