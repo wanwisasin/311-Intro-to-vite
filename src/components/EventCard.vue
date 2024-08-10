c70e1949ec4075f571aa7efb00dead8e2c701678
wanwisasin <Wanwisa_j@cmu.ac.th>
2024-08-10 15:09:58 +0700

    lab6 8

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -19,7 +19,9 @@ defineProps<{
 
 <template>
   <RouterLink class="event-link" :to="{ name: 'event-detail-view', params: { id: event.id } }">
-    <div class="event-card hover:scale-101 hover:shadow-sp">
+    <div
+      class="cursor-pointer border border-gray-600 p-[20px] w-[250px] mb-[18px] hover:scale-101 hover:shadow-sp"
+    >
       <h2>{{ event.title }}</h2>
       <span>@ {{ event.time }} on {{ event.date }}</span>
     </div>
@@ -27,13 +29,13 @@ defineProps<{
 </template>
 
 <style scoped>
-.event-card {
+/* .event-card {
   padding: 20px;
   width: 250px;
   cursor: pointer;
   border: 1px solid #39495c;
   margin-bottom: 18px;
-}
+} */
 /* .event-card:hover {
   transform: scale(1.01);
   box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
